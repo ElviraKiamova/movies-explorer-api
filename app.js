3f96543ac27defcfbdf1bba22fb5022103714f47
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-11-08 11:11:54 +0500

    feat: add app,  install body-parser, express, mongoose

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.listen(PORT);
