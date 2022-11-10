12f143d1a623e0b0a8ed48dc39bbc4015bc7ef15
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-11-10 14:47:11 +0500

    feat: add additional error handlers

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const auth = require('./middlewares/auth');
 const { createUser, login } = require('./controllers/users');
 const { registerValid, loginValid } = require('./middlewares/validation');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const notFoundController = require('./controllers/notFoundController');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -38,8 +40,12 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/movies', require('./routes/movies'));
 
+app.use(notFoundController);
+
 app.use(errorLogger);
 
 app.use(errors());
+
+app.use(errorHandler);
 
 app.listen(PORT);
