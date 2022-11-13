2c22d282a778d2a22aa7958af6a4eb91b48dc17a
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-11-13 05:12:04 +0500

    fix: correct the validation of the login request

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -21,7 +21,6 @@ const loginValid = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().required().min(2).max(30),
   }),
 });
 
