b272cb4414f3a97a55026744751f0763b49b0f3a
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-11-14 11:09:38 +0500

    fix: handling a non-existent address error

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@ const users = require('./users');
 const movies = require('./movies');
 const authorizRout = require('./authorizRout');
 const auth = require('../middlewares/auth');
+const NotFound = require('../errors/NotFound');
 
 module.exports = function (app) {
   app.use('/', authorizRout);
   app.use(auth);
   app.use('/users', users);
   app.use('/movies', movies);
+
+  app.all('*', (req, res, next) => {
+    next(new NotFound('Обращение по несуществующему адресу'));
+  });
 };
